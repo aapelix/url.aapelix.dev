22a591255c4f871d4636a4553533d8e4336bddf2
aapelix <mcaapelix@gmail.com>
2024-12-28 14:49:47 +0200

    more build errors

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,15 +1,11 @@
-{/* <div className='font-mono flex w-screen h-screen items-center justify-center'>
-        <h1 className="text-6xl text-center font-bold font-mono">Redirecting you to {redirectUrl}</h1>
-      </div>
-
-<div className='font-mono flex w-screen h-screen items-center justify-center'>
-        <p className='text-6xl font-bold'>URL not found</p>
-      </div> */}
-
 import { redirect } from 'next/navigation';
 import { createClient } from '@/utils/supabase/server';
 
-export default async function RedirectPage({ params }: { params: { id: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
 
   console.log(id)
